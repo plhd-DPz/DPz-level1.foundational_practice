e11d62bca9fa056753324cc560c98f68b0cfceea
DPz <plhd148@gmail.com>
2026-03-19 07:24:05 +0700

    Add files via upload

---
+++ se.cit.ctu.edu.vn bt/Mảng/đếm số nguyên lẻ.c
@@ -0,0 +1,43 @@
+/*
+Viết chương trình nhập vào n số nguyên và cho biết những số nguyên lẻ trong đã nhập
+
+Đầu vào
+
+Dòng đầu tiên là n - số lượng các số nguyên (0 < n 
+ 1000).
+Dòng thứ hai chứa n số nguyên, mỗi số cách nhau khoảng trắng
+Đầu ra
+
+Dòng đầu tiên là n số nguyên đã nhập, mỗi số cách nhau khoảng trắng
+Dòng thứ 2 là số lượng các số nguyên lẻ 
+Dòng thứ 3 là các số nguyên lẻ đã nhập
+
+
+Ghi chú:
+
+Dữ liệu đầu vào là hợp lệ.
+Bạn không nên dùng printf trước scanf.
+*/
+#include <stdio.h>
+int main(){
+	int n, i;
+	scanf("%d",&n);
+	int a[n];
+	for (i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	int count=0, odd[n], j=0;
+	for (i=0;i<n;i++){
+		printf("%d ",a[i]);
+		if (a[i]%2!=0){
+			count++;
+			odd[j]=a[i];
+			j++;
+		}
+	}
+	printf("\n%d\n",count);
+	for (i=0;i<count;i++){
+		printf("%d ",odd[i]);
+	}
+	return 0;
+}
