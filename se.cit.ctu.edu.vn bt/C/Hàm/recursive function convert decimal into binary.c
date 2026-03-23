d9a04bf6dab261ddb62a8a2fb465a61ef5b9e4e9
DPz <plhd148@gmail.com>
2026-03-23 22:35:55 +0700

    Create recursive function convert decimal into binary.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/recursive function convert decimal into binary.c
@@ -0,0 +1,19 @@
+/*
+Write a recursive function that converts a decimal number into a binary number and prints the binary number.
+
+Prototype
+
+function name: dec2bin
+parameters:
+an integer n - the number (1 <= n <= 64)
+return type: none
+Body
+
+recursively print the binary bits of n.
+*/
+
+void dec2bin(int n){
+    if (n==0) return;
+    dec2bin(n/2);
+    printf("%d", n%2);
+}
