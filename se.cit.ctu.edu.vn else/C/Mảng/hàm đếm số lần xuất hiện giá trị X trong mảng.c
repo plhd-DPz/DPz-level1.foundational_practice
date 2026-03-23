04ac3a25efd0d44834103164713545df995d3e32
DPz <plhd148@gmail.com>
2026-03-23 22:55:59 +0700

    Create hàm đếm số lần xuất hiện giá trị X trong mảng.c

---
+++ se.cit.ctu.edu.vn else/C/Mảng/hàm đếm số lần xuất hiện giá trị X trong mảng.c
@@ -0,0 +1,25 @@
+/*
+Viết 1 hàm cho phép đếm số lần xuất hiện của giá trị X trong mảng số nguyên A gồm n phần tử.
+
+Khuôn dạng (Prototype)
+
+Tên hàm (function name): countX()
+Tham số (parameters):
+x - kiểu int
+A[] - kiểu int
+n - kiểu int (0<=n<=1000)
+Kiểu trả về (return type): int
+Thân hàm (Body)
+
+Tiến hành duyệt qua mảng A, mỗi lần gặp một giá trị x thì tăng biến đếm lên; cuối cùng biến đếm là kết quả trả về của hàm.
+*/
+
+int countX(int x,int a[], int n){
+	int i, count=0;
+	for (i=0;i<n;i++){
+		if (a[i]==x){
+			count++;
+		}
+	}
+	return count;
+}
