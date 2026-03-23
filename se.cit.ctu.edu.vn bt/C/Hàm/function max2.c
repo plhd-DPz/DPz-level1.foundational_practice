e992dc828a1769fada128b245418d0fdc39ecd6a
DPz <plhd148@gmail.com>
2026-03-23 22:47:01 +0700

    Create function max2.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/function max2.c
@@ -0,0 +1,17 @@
+/*
+Write a C function that takes 2 integer numbers and returns the bigger.
+
+Prototype
+
+function name: max2
+parameters: two integers a and b
+return type: integer
+Body
+
+return the bigger between a and b.
+*/
+
+int max2(int a, int b){
+    if (a>b) return a;
+    return b;
+}
