f4c6063be379e2b15c4eb8133b4ed891cacfdcd0
DPz <plhd148@gmail.com>
2026-03-23 22:42:52 +0700

    Create hàm tính n!!.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/hàm tính n!!.c
@@ -0,0 +1,29 @@
+/*
+Viết hàm tính n!! (0<=n<=25) theo công thức sau:
+n!!=1*3*5*...*n, n lẻ; 2*4*6*...*n, n chẵn
+Khuôn dạng (Prototype)
+
+Tên hàm (function name): Factorial2
+Tham số (parameters):
+n  - kiểu int
+Kiểu trả về (return type): long long
+Thân hàm (Body)
+
+Áp dụng công thức trên để tính giá trị của n!!
+*/
+
+long long Factorial2(int n){
+    int i;
+    long long fac=1;
+    if (n%2==0){
+        for (i=2;i<=n;i+=2){
+            fac*=i;
+        }
+    }
+    else{
+        for (i=3;i<=n;i+=2){
+            fac*=i;
+        }
+    }
+    return fac;
+}
