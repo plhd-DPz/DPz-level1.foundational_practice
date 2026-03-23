af686f1e264c650012accd90d54d3245b0e91338
DPz <plhd148@gmail.com>
2026-03-23 22:43:43 +0700

    Create đệ quy đếm số chữ số n.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/đệ quy đếm số chữ số n.c
@@ -0,0 +1,18 @@
+/*
+Viết hàm đệ quy đếm số chữ số của số nguyên dương n > 0.
+
+Khuôn dạng (Prototype)
+
+Tên hàm (function name): count
+Tham số (parameters):
+n - kiểu int (n > 0)
+Kiểu trả về (return type): int
+Thân hàm (Body)
+
+Tìm số chữ số của n.
+*/
+
+int count(int n){
+    if (n==0) return 0;
+    return count(n/10)+1;
+}
