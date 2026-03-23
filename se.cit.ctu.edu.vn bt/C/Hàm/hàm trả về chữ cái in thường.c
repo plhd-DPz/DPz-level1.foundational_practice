96814d1f8e15755562f4e280924d774fb5d4748d
DPz <plhd148@gmail.com>
2026-03-23 22:36:53 +0700

    Create hàm trả về chữ cái in thường.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/hàm trả về chữ cái in thường.c
@@ -0,0 +1,18 @@
+/*
+Viết hàm nhận vào một chữ ký tự c và trả về chữ cái in thường của ký tự c.
+
+Khuôn dạng (Prototype)
+
+Tên hàm (function name): lowCase
+Danh sách tham số (parameters): c, kiểu ký tự.
+Kiểu trả về (return type): ký tự
+Thân hàm (Body)
+
+Nếu c là chữ cái in HOA (A-Z) thì trả về chữ in thường của nó.
+Ngược lại, trả về c.
+*/
+
+char lowCase(char c){
+    if (!(c>='A' && c<='Z')) return c;
+    else return c+'a'-'A';
+}
