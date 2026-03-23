b94e7c54631c03c47ad1122206b4e7aad6f7d86b
DPz <plhd148@gmail.com>
2026-03-23 18:44:12 +0700

    Create recursive function power.c

---
+++ se.cit.ctu.edu.vn bt/Hàm/recursive function power.c
@@ -0,0 +1,26 @@
+/*Write a recursive function that computes an = a * a * ... * a (the product of multiplying n a's).
+
+Example:
+
+23    = 8
+34    = 81
+210   = 1024
+Prototype
+
+function name: power
+parameters:
+a - the base, type: float (0 <= |a| <= 5)
+n - the exponent, type: int (0 <= n <= 10)
+return type: double
+Body
+
+recursively compute an- the nth power of a.
+Notes:
+
+Input is always valid.
+See examples for more details in input/output.*/
+
+double power(float a, int n){
+    if (n==0) return 1;
+    return power(a, n-1)*a;
+}
