57aa92528835c0c4fbeb95400cedbe1094313e93
DPz <plhd148@gmail.com>
2026-03-23 19:08:56 +0700

    Create recursive function NumDigits.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/recursive function NumDigits.c
@@ -0,0 +1,22 @@
+/*
+Write a recursive function that computes the number of digits of a number.
+
+Prototype
+
+function name: NumDigits
+parameters:
+an integer n - the number (1 <= n <= 106)
+return type: int
+Body
+
+recursively compute the number of digits of n.
+*/
+
+#include <stdlib.h>
+int NumDigits(int n){
+    n=abs(n);
+    if (n<10) return 1;
+    return NumDigits(n/10)+1;
+}
+
+//n có thể âm->cần abs
