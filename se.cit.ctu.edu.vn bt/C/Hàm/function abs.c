2acd882e4666a7b508cdc9cd76ac8740b0a9bfd1
DPz <plhd148@gmail.com>
2026-03-23 22:44:26 +0700

    Create function abs.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/function abs.c
@@ -0,0 +1,17 @@
+/*
+Write a C function that computes the absolute value of a number.
+
+Prototype
+
+function name: my_abs
+parameters: a float number x
+return type: float
+Body
+
+return the absolute value of x
+*/
+
+float my_abs(float x){
+    if (x>=0) return x;
+    return -x;
+}
