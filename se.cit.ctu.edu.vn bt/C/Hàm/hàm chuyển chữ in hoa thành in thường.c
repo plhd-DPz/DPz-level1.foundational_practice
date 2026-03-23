ac7cd1a3142d6874d159f9bee497e42314b54c8f
DPz <plhd148@gmail.com>
2026-03-23 20:24:39 +0700

    Create hàm chuyển chữ in hoa thành in thường.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/hàm chuyển chữ in hoa thành in thường.c
@@ -0,0 +1,16 @@
+/*
+Viết hàm nhận vào một chữ cái in HOA (A-Z) và trả về chữ cái in thường.
+
+Khuôn dạng (Prototype)
+
+Tên hàm (function name): lowCase
+Danh sách tham số (parameters): c, kiểu ký tự.
+Kiểu trả về (return type): ký tự
+Thân hàm (Body)
+
+Tìm và trả về chữ in thường của c.
+*/
+
+char lowCase(char c){
+    return c+'a'-'A';
+}
