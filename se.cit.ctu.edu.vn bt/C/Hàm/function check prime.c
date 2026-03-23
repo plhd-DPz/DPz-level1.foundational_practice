691e8f7f77a30df903b08d2cdbacf50ad4a99ff3
DPz <plhd148@gmail.com>
2026-03-23 19:00:20 +0700

    Create function check prime.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/function check prime.c
@@ -0,0 +1,24 @@
+/*
+Write a C function that check whether a number is prime or not.
+
+Prototype
+
+function name: checkPrime
+parameters: an integer p - the number to be checked (2 <= p <= 1012).
+return type: integer
+Body
+
+check whether p is a prime
+return 1 if p is a prime, otherwise return 0
+*/
+
+#include <math.h>
+int checkPrime(long long p){
+    for (int i=2;i<=sqrt(p);i++){
+        if (p%i==0) return 0;
+    }
+    return 1;
+}
+
+//nếu cần dùng math.h thì include vào, mặc định không có
+//đọc kĩ giới hạn, dùng sqrt chạy vòng for để không bị tle(chạy quá thời gian giới hạn)
