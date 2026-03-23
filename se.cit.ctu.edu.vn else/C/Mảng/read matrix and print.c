36d5cac9a53d04f42f06090efede7701013ef3bb
DPz <plhd148@gmail.com>
2026-03-23 22:58:24 +0700

    Create read matrix and print.c

---
+++ se.cit.ctu.edu.vn else/C/Mảng/read matrix and print.c
@@ -0,0 +1,30 @@
+/*
+Write a program to read a matrix of integer numbers and print it to the screen.
+
+Input
+
+The first line contains m and n,  separated by a space - m: number of rows, n: number of columns (0 < m, n <= 50).
+m following lines, each contains n numbers, separated by a space.
+Output
+
+print the matrix to the screen.
+*/
+
+#include <stdio.h>
+int main(){
+	int m, n, i, j;
+	scanf("%d%d",&m,&n);
+	int a[m][n];
+	for (i=0;i<m;i++){
+		for (j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for (i=0;i<m;i++){
+		for (j=0;j<n;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
