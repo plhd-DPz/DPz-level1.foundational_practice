22b8fddd44c16d91d5a8eb36ad3b9ec3a2282d82
DPz <plhd148@gmail.com>
2026-03-23 22:50:32 +0700

    Create hàm in n kí tự.c

---
+++ se.cit.ctu.edu.vn bt/C/Hàm/hàm in n kí tự.c
@@ -0,0 +1,22 @@
+/*
+Viết hàm để in n ký tự c ra màn hình.
+
+Khuôn dạng (Prototype)
+
+Tên hàm (function name): print_c
+Tham số (parameters):
+c - Ký tự cần in, kiểu char.
+n - kiểu int.
+Kiểu trả về (return type): không có
+Thân hàm (Body)
+
+In n ký tự c liên tiếp nhau ra màn hình.
+*/
+
+void print_c(char c,int n){
+    int i;
+    for (i=0;i<n;i++){
+        printf("%c",c);
+    }
+    return;
+}
