17a8d10a329db7ea9ac32d1d5beef789890f0fb8
plhd-DPz <plhd148@gmail.com>
2026-06-02 22:05:28 +0700

    add

--- code tính năng/printskip2.py
+++ code tính năng/printskip2.py
@@ -1,23 +1,28 @@
-"tác dụng: hỗ trợ việc in tuần tự, 2 mặt, mỗi mặt 2 slide powerpoint để tiết kiệm giấy"
+"tác dụng: hỗ trợ việc in tuần tự nhiều slide powerpoint vào một mặt giấy, in 2 mặt của 1 tờ để tiết kiệm giấy"
+
+
+def generate_pages(total_pages, start=1, per_side=2):
+    pages = []
+    step = per_side * 2
+    i = start
+
+    while i <= total_pages:
+        # Lấy các trang cho 1 mặt
+        for j in range(per_side):
+            if i + j <= total_pages:
+                pages.append(str(i + j))
+        
+        # Nhảy sang tờ tiếp theo
+        i += step
+
+    return pages
 
 
 total_pages = int(input("Nhập tổng số trang: "))
 start = int(input("Nhập số bắt đầu: "))
+per_side = int(input("Nhập số trang mỗi mặt: "))
 
-pages = []
-i = start
-
-while i <= total_pages:
-    # Lấy trang i
-    pages.append(str(i))
-    # Nếu còn trang tiếp theo thì lấy luôn i+1
-    if i + 1 <= total_pages:
-        pages.append(str(i+1))
-    # Bỏ qua 2 trang tiếp theo
-    i += 4
-
-# In kết quả
-print(", ".join(pages))
-
+result = generate_pages(total_pages, start, per_side)
+print(",".join(result))
 
 
