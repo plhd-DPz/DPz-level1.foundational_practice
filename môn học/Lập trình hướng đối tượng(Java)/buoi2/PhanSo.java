f6ccb1040b7807991f29ef652e44036569a1bebe
ABC <plhd148@gmail.com>
2026-06-14 02:32:32 +0700

    them bt

---
+++ môn học/Lập trình hướng đối tượng(Java)/buoi2/PhanSo.java
@@ -0,0 +1,123 @@
+package buoi2;
+
+import java.util.Scanner;
+
+public class PhanSo {
+	private int tuSo, mauSo;
+	public PhanSo() {
+		tuSo=0;
+		mauSo=1;
+	}
+	public PhanSo(int tuSo1, int mauSo1) {
+		if (mauSo1==0) {
+			System.out.println("Mau so khong hop le.");
+			tuSo=0;
+			mauSo=1;
+		}
+		else {
+			tuSo=tuSo1;
+			mauSo=mauSo1;
+		}
+	}
+	public PhanSo(PhanSo a) {
+		tuSo=a.tuSo;
+		mauSo=a.mauSo;
+	}
+	public void in() {
+		if (tuSo==0) System.out.println("0\n");
+		else if (mauSo==1) System.out.println(tuSo);
+		else {
+			if(tuSo*mauSo < 0) System.out.println(Math.abs(tuSo)+"/"+Math.abs(mauSo));
+			else System.out.println(Math.abs(tuSo)+"/"+Math.abs(mauSo));
+		}
+	}
+	public String toString() {
+		if (tuSo==0) return "Phan so la: 0\n";
+		else if (mauSo==1) return "Phan so la: "+ tuSo;
+		else {
+			if(tuSo*mauSo < 0) return "Phan so la: -"+Math.abs(tuSo)+"/"+Math.abs(mauSo);
+			else return "Phan so la: "+Math.abs(tuSo)+"/"+Math.abs(mauSo);
+		}
+	}
+	public void nhap() {
+		Scanner sc=new Scanner(System.in);
+		do {
+			System.out.print("Nhap tu so: ");
+			tuSo=sc.nextInt();
+			System.out.print("Nhap mau so: ");
+			mauSo=sc.nextInt();
+			if (mauSo==0) System.out.println("Mau so phai khac 0, hay nhap lai: ");
+		}while(mauSo==0);
+	}
+	public void nghichDao() {
+		if (tuSo==0) {
+			System.out.println("Khong the nghich dao vi tu so bang 0");
+			return;
+		}
+		int temp=tuSo;
+		tuSo=mauSo;
+		mauSo=temp;
+	}
+	public PhanSo giaTriNghichDao() {
+		if (tuSo==0) {
+			System.out.println("Khong the nghich dao vi tu so bang 0");
+			return new PhanSo(this);
+		}
+		PhanSo a=new PhanSo();
+		a.tuSo=mauSo;
+		a.mauSo=tuSo;	
+		return a;
+	}
+	public float giaTriThuc() {
+		return tuSo/(float)mauSo;
+	}
+	public boolean lonHon(PhanSo a) {
+		if (tuSo/(float)mauSo > a.tuSo/(float)a.mauSo) return true;
+		else return false;
+	}
+	public int gcd(int x, int y) {
+	    while (y != 0) {
+	        int r = x % y;
+	        x = y;
+	        y = r;
+	    }
+	    return Math.abs(x);
+	}
+	public int mauSoChungNhoNhat(PhanSo a) {
+		int x=mauSo, y=a.mauSo;
+		return x / gcd(x, y) * y;
+	}
+	public void rutGon() {
+	    int g = gcd(Math.abs(tuSo), Math.abs(mauSo));
+	    tuSo /= g;
+	    mauSo /= g;
+	}
+	public PhanSo cong(PhanSo a) {
+		PhanSo t=new PhanSo();
+		t.mauSo=mauSoChungNhoNhat(a);
+		t.tuSo=tuSo*(t.mauSo/mauSo)+a.tuSo*(t.mauSo/a.mauSo);
+		//t.rutGon();
+		return t;
+	}
+	public PhanSo tru(PhanSo a) {
+		PhanSo t=new PhanSo();
+		t.mauSo=mauSoChungNhoNhat(a);
+		t.tuSo=tuSo*(t.mauSo/mauSo)-a.tuSo*(t.mauSo/a.mauSo);
+		//t.rutGon();
+		return t;
+	}
+	public PhanSo nhan(PhanSo a) {
+		PhanSo t=new PhanSo();
+		t.tuSo=tuSo*a.tuSo;
+		t.mauSo=mauSo*a.mauSo;
+		//t.rutGon();
+		return t;
+	}
+	public PhanSo chia(PhanSo a) {
+		if (a.tuSo == 0) {
+		    System.out.println("Khong the chia cho 0");
+		    return new PhanSo(this);
+		}
+		return nhan(a.giaTriNghichDao());
+	}
+}
---
+++ môn học/Lập trình hướng đối tượng(Java)/buoi2/SDPhanSo.java
@@ -0,0 +1,55 @@
+package buoi2;
+
+import java.util.Scanner;
+
+public class SDPhanSo {
+
+	public static void main(String[] args) {
+		PhanSo a=new PhanSo(3,7);
+		System.out.print("Phan so a: "+a);
+		PhanSo b=new PhanSo(4, 9);
+		System.out.print("Phan so b: "+b);
+		PhanSo x=new PhanSo();
+		System.out.print("Nhap phan so x: ");
+		x.nhap();
+		PhanSo y=new PhanSo();
+		System.out.print("Nhap phan so y: ");
+		y.nhap();
+		System.out.print("Gia tri nghich dao cua phan so x: "+x.giaTriNghichDao());
+		System.out.print("Tong cua phan so x va y la: "+x.cong(y));
+		PhanSo ds[];
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Nhap so phan tu: ");
+		int n=sc.nextInt();
+		ds=new PhanSo[n];
+		for (int i=0;i<n;i++) {
+			ds[i]=new PhanSo();
+			System.out.println("Nhap phan so thu "+(i+1)+": ");
+			ds[i].nhap();
+		}
+		PhanSo max=ds[0];
+		PhanSo tong=ds[0];
+		for (int i=1;i<n;i++) {
+			tong=tong.cong(ds[i]);
+			if (max.giaTriThuc()<ds[i].giaTriThuc()) max=ds[i];
+		}
+		System.out.print("Tong cac phan so la: "+tong);
+		System.out.println("Phan so lon nhat la: "+max);
+		
+		tong.in();
+		for (int i=0;i<n-1;i++) {
+		    for (int j=i+1;j<n;j++) {
+		        if (ds[i].giaTriThuc() > ds[j].giaTriThuc()) {
+		            PhanSo temp=ds[i];
+		            ds[i]=ds[j];
+		            ds[j]=temp;
+		        }
+		    }
+		}
+		System.out.println("Danh sach phan so sau khi sap xep: ");
+		for (PhanSo e : ds) {
+			e.in();
+		}
+	}
+
+}
