ae982ead294b62eb7e99457f0a6246ceeb077a25
DPz <plhd148@gmail.com>
2026-06-15 15:46:03 +0700

    Update Date.java

--- môn học/Lập trình hướng đối tượng(Java)/buoi2/Date.java
+++ môn học/Lập trình hướng đối tượng(Java)/buoi2/Date.java
@@ -2,57 +2,139 @@ package buoi2;
 
 import java.util.Scanner;
 
-public class Diem {
-	private int x,y;
-	public Diem() {
-		x=0;
-		y=0;
+public class Date {
+	private int d, m, y;
+	public Date() {
+		d=1;
+		m=1;
+		y=2026;
 	}
-	public Diem(int n) {
-		x=y=n;
-	}
-	public Diem(int x1, int y1) {
-		x=x1;
+	public Date(int d1, int m1, int y1) {
+		d=d1;
+		m=m1;
 		y=y1;
+		if (!hopLe()) {
+			System.out.println("Ngay duoc truyen vao khong hop le!");
+			d=1;
+			m=1;
+			y=2026;
+		}
 	}
-	public Diem(Diem a) {
-		x=a.x;
+	public Date(Date a) {
+		d=a.d;
+		m=a.m;
 		y=a.y;
 	}
-	public void nhapDiem() {
-		Scanner sc=new Scanner(System.in);
-		System.out.print("\nNhap toa do x = ");
-		x=sc.nextInt();
-		System.out.print("\nNhap toa do y = ");
-		y=sc.nextInt();
-	}
-	public void hienThi() {
-		System.out.println("("+x+","+y+")");
+	public void in() {
+		System.out.println(d+"/"+m+"/"+y);
 	}
 	public String toString() {
-		return "("+x+","+y+")";
+		return d+"/"+m+"/"+y;
 	}
-	public void gan(int x1, int y1) {
-		x=x1;
-		y=y1;
+	public void nhap() {
+		Scanner sc=new Scanner(System.in);
+		do {
+			System.out.print("Nhap ngay: ");
+			d=sc.nextInt();
+			System.out.print("Nhap thang: ");
+			m=sc.nextInt();
+			System.out.print("Nhap nam: ");
+			y=sc.nextInt();
+			if (!hopLe()) System.out.println("Ngay khong hop le, vui long nhap lai!");
+		} while (!hopLe());
 	}
-	public void doiDiem(int dx, int dy) {
-		x=x+dx;
-		y=y+dy;
+	public int layNgay() {
+		return d;
 	}
-	public float khoangCach() {
-		return (float)Math.sqrt(x*x+y*y);
+	public int layThang() {
+		return m;
 	}
-	public float khoangCach(Diem d) {
-		return (float)Math.sqrt(Math.pow(x-d.x,2)+Math.pow(y-d.y,2));
-	}
-	public int giaTriX() {
-		return x;
-	}
-	public int giaTriY() {
+	public int layNam() {
 		return y;
 	}
-	public void TrungDiem(Diem b) {
-		System.out.println("Trung diem A, B co toa do: "+(x+b.x)/2.0+","+(y+b.y)/2.0);
+	public boolean hopLe() {
+		int max[]= {0,31,28,31,30,31,30,31,31,30,31,30,31};
+		if ((y%4==0 && y%100!=0) || y%400==0) max[2]=29;
+		if (d>0 && m>0 && y>0 && m<13 && d<=max[m]) return true;
+		else return false;
+	}
+	public Date ngayHomSau() {
+		Date a=new Date(this);
+		a.d++;
+		if(!a.hopLe()) {
+			a.m++;
+			a.d=1;
+			if(!a.hopLe()) {
+				a.y++;
+				a.m=1;
+			}
+		}
+		return a;
+	}
+	public Date congNgay(int n) {
+		Date a=new Date(d,m,y);
+		for (int i=0;i<n;i++) {
+			a=a.ngayHomSau();
+		}
+		return a;
+	}
+	public boolean ngayTruoc(Date a) {
+		if (y!=a.y) return y<a.y;
+		if (m!=a.m) return m<a.m;
+		return d<a.d;
+	}
+	public boolean equals(Date a) {
+	    return d == a.d && m == a.m && y == a.y;
+	}
+	public int khoangCachTuMoc() {
+	    Date moc = new Date(8,6,2026);     //biết 8/6/2026 là thứ 2
+	    if (this.equals(moc))
+	        return 0;
+	    int dem = 0;
+	    if (moc.ngayTruoc(this)) {
+	        Date a = new Date(moc);
+	        while (!a.equals(this)) {
+	            a = a.ngayHomSau();
+	            dem++;
+	        }
+	        return dem;
+	    }
+	    Date a = new Date(this);
+	    while (!a.equals(moc)) {
+	        a = a.ngayHomSau();
+	        dem++;
+	    }
+	    return -dem;
+	}
+	public String thu() {
+	    String t[] = {"", "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay", "Chu nhat"};
+	    int thu = ((khoangCachTuMoc() % 7) + 7) % 7 + 1; //quy ước Thứ 2 là 1
+	    return t[thu];
+	}
+	public String thu(int n) {  //n ngày nữa là thứ mấy tính từ ngày 8/6/2026
+		String t[] = {"", "Thu hai", "Thu ba", "Thu tu", "Thu nam", "Thu sau", "Thu bay", "Chu nhat"};
+	    int k = ((n % 7) + 7) % 7 + 1;
+	    return t[k];
+	}
+	public int soNgayTrongThang() {
+	    int max[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+	        max[2] = 29;
+	    return max[m];
+	}
+	public void inThang() {
+	    int thuDau = khoangCachTuMoc();
+	    thuDau = ((thuDau % 7) + 7) % 7 + 1;
+	    System.out.println("\nTHANG " + m + "/" + y);
+	    System.out.println(" T2  T3  T4  T5  T6  T7  CN");
+	    for (int i = 1; i < thuDau; i++)
+	        System.out.print("    ");
+	    int sn = soNgayTrongThang();
+	    for (int i = 1; i <= sn; i++) {
+	        System.out.printf("%3d ", i);
+	        if ((i + thuDau - 1) % 7 == 0)
+	            System.out.println();
+	    }
+	    System.out.println();
 	}
 }
