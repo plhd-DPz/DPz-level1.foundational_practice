532183aea5c088610cc508c8f56e6e6c03db221a
DPz <plhd148@gmail.com>
2026-06-29 15:54:22 +0700

    Update SDSinhVien.java

--- môn học/Lập trình hướng đối tượng(Java)/buoi3/SDSinhVien.java
+++ môn học/Lập trình hướng đối tượng(Java)/buoi3/SDSinhVien.java
@@ -1,42 +1,43 @@
 package buoi3;
 
-import buoi2.Date;
-
 import java.util.Scanner;
 
 public class SDSinhVien {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		SinhVien a=new SinhVien();
 		System.out.println("Nhap thong tin sinh vien a: ");
 		a.nhap();
+		System.out.println("Thong tin sinh vien a la: ");
 		a.in();
-		System.out.println("Sinh vien a dang ki them mon LTHDT.");
+		System.out.println("\nSinh vien a dang ki them mon LTHDT.");
 		a.them("LTHDT");
 		a.ganDiem("LTHDT", "A");
-		System.out.println("Thong tin sinh vien a la: "+a);
-		a.xoa("LTHDT");
-		System.out.println("Thong tin sinh vien a sau khi xoa LTHDT: "+a);
+		System.out.println("Thong tin sinh vien a sau khi dang ki them mon LTHDT la: "+a+"\n");
+		System.out.print("Nhap mon hoc sinh vien a can xoa: ");
+		String x=sc.nextLine();
+		a.xoa(x);
+		System.out.println("Thong tin sinh vien a sau khi xoa mon "+x+" la: "+a);
 		SinhVien ds[];
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Nhap so sinh vien trong danh sach: ");
+		System.out.print("Nhap so luong sinh vien trong danh sach: ");
 		int n=sc.nextInt();
 		ds=new SinhVien[n];
 		for (int i=0;i<n;i++) {
-			System.out.println("Nhap sinh vien thu "+(i+1)+": ");
+			System.out.println("Nhap thong tin sinh vien thu "+(i+1)+": ");
 			ds[i]=new SinhVien();
 			ds[i].nhap();
 		}
-		System.out.println("Danh sach sinh vien bi canh cao hoc vu: ");
-		for (int i=0;i<n;i++) 
+		System.out.println("Danh sach cac sinh vien bi canh cao hoc vu: ");
+		for (int i=0;i<n;i++)
 			if (ds[i].diemTB()<1) ds[i].in();
-		System.out.println("Danh sach sinh vien co diem cao nhat: ");
+		System.out.println("\nDanh sach sinh vien co diem trung binh cao nhat: ");
 		float max=ds[0].diemTB();
 		for (int i=1;i<n;i++)
 			if (ds[i].diemTB()>max) max=ds[i].diemTB();
 		for (int i=0;i<n;i++)
 			if (ds[i].diemTB()==max) ds[i].in();
-		System.out.println("Sap xep danh sach sinh vien theo ten: ");
+		System.out.println("\nSap xep cac sinh vien theo ten: ");
 		for (int i=0;i<n-1;i++) {
 			for (int j=i+1;j<n;j++) {
 				if (ds[i].layTen().compareTo(ds[j].layTen())>0) {
@@ -46,34 +47,21 @@ public class SDSinhVien {
 				}
 			}
 		}
-		for (int i=0;i<n;i++) ds[i].in();
+		for (SinhVien e : ds) e.in();
 		int kha=0, gioi=0, xuatSac=0;
-		for (int i = 0; i < n; i++) {
-		    String xl = ds[i].xepLoai();
-		    if (xl.equals("Kha"))
-		        kha++;
-		    else if (xl.equals("Gioi"))
-		        gioi++;
-		    else if (xl.equals("Xuat sac"))
-		        xuatSac++;
+		for (int i=0;i<n;i++) {
+			String x1=ds[i].xepLoai();
+			if (x1.equals("Xuat sac")) xuatSac++;
+			else if (x1.equals("Gioi")) gioi++;
+			else if (x1.equals("Kha")) kha++;
 		}
-		System.out.println("So sinh vien kha: "+kha);
-		System.out.println("So sinh vien gioi: "+gioi);
-		System.out.println("So sinh vien xuat sac: "+xuatSac);
-		sc.nextLine();
-		System.out.print("Nhap ten can tim: ");
-		String ten = sc.nextLine();
-		for (int i = 0; i < n; i++) {
-		    if (ds[i].layHoTen().equals(ten))
-		        ds[i].in();
-		}
-		Date d = new Date();
-		System.out.println("Nhap ngay sinh can tim:");
-		d.nhap();
-		for (int i = 0; i < n; i++) {
-		    if (ds[i].layNgaySinh().equals(d))
-		        ds[i].in();
-		}
+		System.out.println("\nSo sinh vien kha trong danh sach: "+kha);
+		System.out.println("So sinh vien gioi trong danh sach: "+gioi);
+		System.out.println("So sinh vien xuat sac trong danh sach: "+xuatSac);
+		System.out.println("\nTim kiem sinh vien theo ho ten: ");
+		SinhVien.timTheoTen(ds, n);
+		System.out.println("\nTim kiem sinh vien theo ngay sinh: ");
+		SinhVien.timTheoNgaySinh(ds, n);
 	}
 
 }
