bfa43d86c3b3effd994b82e6242a85f29223283c
DPz <plhd148@gmail.com>
2026-06-29 15:55:06 +0700

    Update SDDoanThang.java

--- môn học/Lập trình hướng đối tượng(Java)/buoi3/SDDoanThang.java
+++ môn học/Lập trình hướng đối tượng(Java)/buoi3/SDDoanThang.java
@@ -6,35 +6,35 @@ import buoi2.Diem;
 
 public class SDDoanThang {
 
-	public static void main(String[] args) throws CloneNotSupportedException {
+	public static void main(String[] args) {
+		Diem a=new Diem(2,5);
+		Diem b=new Diem(20,35);
+		DoanThang ab=new DoanThang(a,b);
+		System.out.println("Doan thang AB da cho la: "+ab);
+		ab.tinhTien(5, 3);
+		System.out.println("Doan thang AB sau khi tinh tien la: "+ab);
 		DoanThang dt1=new DoanThang();
-		System.out.println("Nhap toa do doan thang: ");
-		dt1.nhap();
-		System.out.print("\nDoan thang da nhap la: ");
-		dt1.in();
-		DoanThang dt2=new DoanThang(new Diem(2,5), new Diem(20,35));
-		DoanThang dt3=new DoanThang(dt2);
-		System.out.println("\nDoan thang AB la: "+dt3);
-		DoanThang dt4=(DoanThang)dt3.clone();
-		dt4.tinhTien(5,3);
-		System.out.println("Doan thang AB sau khi tinh tien la: "+dt4);
-		DoanThang dt5=new DoanThang();
 		System.out.println("Nhap toa do doan thang CD: ");
 		dt1.nhap();
-		dt5.gan(dt1);
-		System.out.println("\nDo dai doan thang CD la: "+dt5.doDai());
-		System.out.println("Goc cua CD voi truc hoanh la: "+dt5.goc());
+		DoanThang dt2=new DoanThang(dt1);     //test cac ham sao chep
+		DoanThang cd=new DoanThang();
+		cd.gan(dt2);
+		System.out.print("Doan thang da nhap la: ");
+		cd.in();
+		System.out.println("\nDo dai doan thang CD la: "+cd.doDai());
+		System.out.println("Goc cua CD voi truc hoanh la: "+cd.goc());
 		DoanThang ds[];
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Nhap so phan tu trong mang: ");
+		System.out.print("Nhap so doan thang trong danh sach: ");
 		int n=sc.nextInt();
 		ds=new DoanThang[n];
 		for (int i=0;i<n;i++) {
-			System.out.println("Nhap phan tu thu "+(i+1)+": ");
+			System.out.println("Nhap doan thang thu "+(i+1)+": ");
 			ds[i]=new DoanThang();
 			ds[i].nhap();
 		}
-		System.out.println("Danh sach vua nhap: ");
+		
+		System.out.println("Danh sach cac doan thang vua nhap la: ");
 		for (DoanThang e : ds) e.in();
 	}
 
